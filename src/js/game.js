982ec41ae778cd51e3f2c0ab785df90c2ea57459
juannunesz <juannuneszbr@gmail.com>
2021-02-13 01:01:56 -0300

    Finalizando projeto

--- src/js/game.js
+++ src/js/game.js
@@ -3,6 +3,20 @@ let largura = 0;
 let vidas = 1;
 let tempo = 15;
 
+let criaMosquitoTempo = 1500
+
+let nivel = window.location.search
+nivel = nivel.replace('?','')
+
+if(nivel === 'normal'){
+  criaMosquitoTempo = 1500
+}else if(nivel === 'dificil'){
+  criaMosquitoTempo = 1000
+}else if(nivel === 'chucknorris'){
+  criaMosquitoTempo = 750
+}
+
+
 function ajustaTamanhoPalcoJogo(){
   altura = window.innerHeight;
   largura = window.innerWidth;
